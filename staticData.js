7a7c110ae99f0d567691a1efdf293417ab08dc2d
JunChoi <97496821+JunChoii@users.noreply.github.com>
2023-02-02 23:40:44 -0800

    Update staticData.js

--- staticData.js
+++ staticData.js
@@ -1,13 +1,10 @@
 
 async function getAllTodos() {
 	let data = [
-		  { todo_id: 1, todo_name: 'buy milk' },
-		  { todo_id: 2, todo_name: 'and cheese' },
-		  { todo_id: 3, todo_name: 'truck in for service' },
-		  { todo_id: 4, todo_name: 'get a hair cut' },
-		{ todo_id: 5, todo_name: 'Call Patrick' },
-		{ todo_id: 6, todo_name: 'Tell him hes wrong' },
-		{ todo_id: 7, todo_name: 'oh no tell him Im wrong' }
+		  { todo_id: 1, todo_name: 'Call Patrick' },
+		  { todo_id: 2, todo_name: 'Tell him hes wrong' },
+		  { todo_id: 3, todo_name: 'Wait, sorry' },
+		  { todo_id: 4, todo_name: 'Tell him Im wrong' }
 		]
 	;
 	return data;
