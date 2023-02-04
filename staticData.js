b75875d3afdf7f84d6c1c22b144e09314fe55dbe
JunChoi <97496821+JunChoii@users.noreply.github.com>
2023-02-03 17:05:42 -0800

    Add files via upload

--- staticData.js
+++ staticData.js
@@ -1,10 +1,10 @@
 
 async function getAllTodos() {
 	let data = [
-		  { todo_id: 1, todo_name: 'Call Patrick' },
-		  { todo_id: 2, todo_name: 'Tell him hes wrong' },
-		  { todo_id: 3, todo_name: 'Wait, sorry' },
-		  { todo_id: 4, todo_name: 'Tell him Im wrong' }
+		  { todo_id: 1, todo_name: 'buy milk' },
+		  { todo_id: 2, todo_name: 'and cheese' },
+		  { todo_id: 3, todo_name: 'truck in for service' },
+		  { todo_id: 4, todo_name: 'get a hair cut' }
 		]
 	;
 	return data;
